1ecbc4d17aaba11eccac9a6321312a6c2983db4b
omarmesa <omarmesagarcia@gmail.com>
2020-12-01 20:29:47 +0100

    1.2 OK

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-$('.dropdown-toggle').dropdown();
+$(document).ready(function(){
+
+    // comprobamos siempre despues de cargar la pagina
+    if (window.location.href.indexOf("?buscador=") > -1) {
+
+        var url = window.location.href;
+        var parametro = url.split("=")[1];
+        var parametro2 = parametro.replace(/[+]/g,' ')
+        $("#resultado").html(parametro2);
+
+    }
+
+    // validamos que el formulario no este en blanco
+    $("#formulario").submit(function( event ) {
+
+        var buscador = $('#buscador').val()
+    
+        if(buscador == "" || buscador.indexOf(" ") == 0){
+            return false;
+        }
+        
+        return true;
+        
+      });
+
+  });
