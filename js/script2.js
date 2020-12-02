ff7796972d4acc5c092653a85c1a21d278b1fb9b
omarmesa <omarmesagarcia@gmail.com>
2020-12-02 09:56:26 +0100

    fetch Ok falta gif

--- js/script2.js
+++ js/script2.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
-
+    
     fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/12047956)')
         .then(response => response.json())
-        .then(data => 
-            console.log(data)
+        .then((data) => {
+            console.log(data);
+            var titulo = data.title;
+            var foto = data.cover;
+            var artista = data.artist.name;
 
             
-    );
-
-
+            $("#album-title").html(titulo);
+            $("#album-artist").html(artista);
+            $("#album-cover").attr("src",foto);
+        })
 
 });
