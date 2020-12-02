fe916150dc47a2c2b5664eb86ebbe002cff7bca6
omarmesa <omarmesagarcia@gmail.com>
2020-12-02 10:38:59 +0100

    Todo ACT1 OK

--- js/script2.js
+++ js/script2.js
@@ -1,17 +1,25 @@
 $(document).ready(function(){
-    
+
+
     fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/12047956)')
         .then(response => response.json())
         .then((data) => {
-            console.log(data);
+            // Guardamos los datos que queremos mostrar
             var titulo = data.title;
             var foto = data.cover;
             var artista = data.artist.name;
 
-            
+            //Borramos el gif de carga
+            $( "#divgif" ).remove();
+
+            // Mostramos los datos por pantalla
             $("#album-title").html(titulo);
             $("#album-artist").html(artista);
             $("#album-cover").attr("src",foto);
         })
+        .catch(function(error) {
+            $( "#loading" ).remove();
+            $("#error").html('Hubo un problema con la petición Fetch:' + error.message);
+          });
 
 });
